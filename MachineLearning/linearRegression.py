99b785b1d79d394da71d3f9760a6c4045dad2d14
Pang KuanXin <pang.kuanxin@cmsholding.com>
2025-11-18 15:36:42 +0800

    Modify the logic

--- MachineLearning/linearRegression.py
+++ MachineLearning/linearRegression.py
@@ -1,10 +1,15 @@
 from sklearn import linear_model
+import numpy as np
 
-train_X = [[1.0], [4.0]]
-train_y = [5.5, 8.5]
+X = np.array([[1.0, 2.0], [4.0, 3.0]])
+y = np.array([5.5, 8.5])
 
 reg = linear_model.LinearRegression()
-reg.fit(train_X, train_y)
+reg.fit(X, y)
 
 print(f"w = {reg.coef_}")
 print(f"b = {reg.intercept_}")
+print(f"score = {reg.score(X, y)}")
+
+target_y = reg.predict(np.array([[6.0, 5.0]]))
+print(f"target_y = {target_y}")
