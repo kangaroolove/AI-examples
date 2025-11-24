c6235ba765f5c22298df0c8c6ce0e1f4ca200052
Pang KuanXin <pang.kuanxin@cmsholding.com>
2025-11-24 17:23:49 +0800

    Remove the useless functions

--- DeepLearning/classification_simpleCNN.py
+++ DeepLearning/classification_simpleCNN.py
@@ -8,60 +8,6 @@ import torchvision
 import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
-from torchvision.utils import make_grid
-
-
-# class OxfordFlowersDataset(Dataset):
-#     def __init__(self, root_dir, transform=None):
-#         self.root_dir = root_dir
-#         self.img_dir = os.path.join(root_dir, 'jpg')
-
-#         labels_mat = scipy.io.loadmat(os.path.join(root_dir, 'imagelabels.mat'))
-
-#         self.labels = labels_mat['labels'][0] - 1
-#         self.transform = transform
-
-#     def __len__(self):
-#         return len(self.labels)
-    
-#     def __getitem__(self, idx):
-#         img_name = f'image_{idx + 1:05d}.jpg'
-#         img_path = os.path.join(self.img_dir, img_name)
-
-#         image = Image.open(img_path)
-#         label = self.labels[idx]
-
-#         if self.transform:
-#             image = self.transform(image)
-#         return image, label
-    
-# def test_loader(dataset):
-#     loader = DataLoader(dataset, batch_size=4, shuffle=True)
-
-#     for images, labels in loader:
-#         print(f"Success! Batch shape: {images.shape}\n")
-#         break
-
-# class TransformSubset(Dataset):
-#     def __init__(self, dataset, indices, transform=None):
-#         self.transform = transform
-#         self.indices = indices
-#         self.dataset = dataset
-
-#     def __len__(self):
-#         return len(self.indices)
-
-#     def __getitem__(self, idx):
-#         img, label = dataset[self.indices[idx]]   # get raw PIL Image / tensor
-#         if self.transform is not None:
-#             img = self.transform(img)
-#         return img, label
-
-    
-# os.makedirs("flower_data", exist_ok=True)
 
 train_transform = transforms.Compose([
     # Radom augmentation
@@ -89,50 +35,25 @@ test_dataset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=False, transform=basic_transform
 )
 
-loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
 
+class itemClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.layers = nn.Sequential(
+            nn.Linear(3 * 224 * 224, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 102)
+        )
 
-# dataset = OxfordFlowersDataset("./flower_data", transform=None)
-# print(f"Total samples: {len(dataset)}")
-
-# train_size = int(0.7 * len(dataset))
-# val_size = int(0.15 * len(dataset))
-# test_size = len(dataset) - train_size - val_size
-
-# train_dataset, val_dataset, test_dataset = random_split(
-#     dataset, [train_size, val_size, test_size]
-# )
-
-# print(f"Training: {len(train_dataset)} images")
-# print(f"Validation: {len(val_dataset)} images")
-# print(f"Test: {len(test_dataset)} images")
-
-# transformed_train_dataset = TransformSubset(dataset, train_dataset.indices, transform=train_transform)
-# transformed_valid_dataset = TransformSubset(dataset, val_dataset.indices, transform=basic_transform)
-# transformed_test_dataset = TransformSubset(dataset, test_dataset.indices, transform=basic_transform)
-
-# batch_size = 32
-# train_loader = DataLoader(transformed_train_dataset, batch_size=batch_size, shuffle=True)
-# val_loader = DataLoader(transformed_valid_dataset, batch_size=batch_size, shuffle=False)
-# test_loader = DataLoader(transformed_test_dataset, batch_size=batch_size, shuffle=False)
-
-# class flowerClassifier(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.flatten = nn.Flatten()
-#         self.layers = nn.Sequential(
-#             nn.Linear(3 * 224 * 224, 128),
-#             nn.ReLU(),
-#             nn.Linear(128, 128),
-#             nn.ReLU(),
-#             nn.Linear(128, 102)
-#         )
-
-#     def forward(self, x) :
-#         x = self.flatten(x)
-#         x = self.layers(x)
-#         return x
+    def forward(self, x) :
+        x = self.flatten(x)
+        x = self.layers(x)
+        return x
     
 # # Check for GPU
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
