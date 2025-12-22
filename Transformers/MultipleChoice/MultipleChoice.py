e38cf20f3356c0764d065f7ef3f36eb864471f04
Pang KuanXin <pang.kuanxin@cmsholding.com>
2025-12-22 17:20:18 +0800

    Submit the MultipleChoice.py

--- Transformers/MultipleChoice/MultipleChoice.py
+++ Transformers/MultipleChoice/MultipleChoice.py
@@ -4,11 +4,78 @@ from datasets import load_dataset
 swag = load_dataset("swag", "regular")
 train_swag_dataset = swag["train"];
 valid_swag_dataset = swag["validation"];
-test_swag_dataset = swag["test"];
+# test_swag_dataset = swag["test"];
 
-train_swag_dataset = train_swag_dataset.select(range(10000));
+train_swag_dataset = train_swag_dataset.select(range(5000));
 print(len(train_swag_dataset))
 print(len(valid_swag_dataset))
-print(len(test_swag_dataset))
+# print(len(test_swag_dataset))
 
 print(train_swag_dataset[0])
+
+model_name = "google-bert/bert-base-uncased"
+
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+ending_names = ["ending0", "ending1", "ending2", "ending3"]
+
+def preprocess_function(examples):
+    first_sentences = [[context] * 4 for context in examples["sent1"]]
+    question_headers = examples["sent2"]
+    second_sentences = [
+        [f"{header} {examples[end][i]}" for end in ending_names] for i, header in enumerate(question_headers)
+    ]
+
+    first_sentences = sum(first_sentences, [])
+    second_sentences = sum(second_sentences, [])
+
+    tokenized_examples = tokenizer(first_sentences, second_sentences, truncation=True)
+    return {k: [v[i : i + 4] for i in range(0, len(v), 4)] for k, v in tokenized_examples.items()}
+
+tokenized_train_swag = train_swag_dataset.map(preprocess_function, batched=True)
+tokenized_validation_swag = valid_swag_dataset.map(preprocess_function, batched=True)
+
+from transformers import DataCollatorForMultipleChoice
+collator = DataCollatorForMultipleChoice(tokenizer=tokenizer)
+
+import evaluate
+
+accuracy = evaluate.load("accuracy")
+
+import numpy as np
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+from transformers import AutoModelForMultipleChoice, TrainingArguments, Trainer
+
+model = AutoModelForMultipleChoice.from_pretrained(model_name)
+
+training_args = TrainingArguments(
+    output_dir="my_awesome_swag_model",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    learning_rate=5e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    push_to_hub=False,
+    bf16=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train_swag,
+    eval_dataset=tokenized_validation_swag,
+    processing_class=tokenizer,
+    data_collator=collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
